6651315aa30f81d5eb42f08c8f55bbacffd13ac5
Anthony <anthony.cuttivet@gmail.com>
2019-12-08 15:22:31 +0100

    Countdown + sounds

---
+++ Assets/Scripts/Minigames/Deceived/CountDown.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class CountDown : MonoBehaviour
+{
+
+    public float time;
+    public AudioClip countDownSFX;
+    public AudioClip goSFX;
+
+    private int currentSecond = -1;
+
+    // Start is called before the first frame update
+    void Start(){
+        currentSecond = (int)time % 60;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(!CharactersSpawner.instance.gameStart){
+            if(time > 0){
+                time -= Time.deltaTime;
+                int second = (int)time % 60;
+                gameObject.GetComponent<TextMeshProUGUI>().text = (second + 1).ToString();
+                if(currentSecond != second){
+                    PlaySound(countDownSFX);
+                }
+                currentSecond = second;
+            }else{
+                CharactersSpawner.instance.gameStart = true;
+                //gameObject.GetComponent<DestroySelf>().enabled = true;
+                gameObject.GetComponent<TextMeshProUGUI>().text = "GO!";
+                PlaySound(goSFX);
+                gameObject.GetComponent<TextMeshProUGUI>().DOFade(0,0.5f);
+            }
+        }
+    }
+
+    void PlaySound(AudioClip clp){
+        gameObject.GetComponent<AudioSource>().clip = clp;
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+
+}
