f6ac9aa31dffe5db993dedb498f577efe87cf373
Anthony <anthony.cuttivet@gmail.com>
2019-12-24 13:14:04 +0100

    Title screen of the seum but working

--- Assets/Scripts/TitleScreen/TitleScreenNavigation.cs
+++ Assets/Scripts/TitleScreen/TitleScreenNavigation.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class TitleScreenNavigation : MonoBehaviour
 {
+
+    public string selectedMenu;
+
     void OnEnable(){
         gameObject.GetComponent<PlayerInput>().actions.Enable();
         PlayerPrefs.SetInt("SP_MONSTER_SKIN", 0);
     }
 
-    void OnA(){
-        SceneManager.LoadScene("CharacterSelection");
+    void Update(){
+        selectedMenu = EventSystem.current.currentSelectedGameObject.name;
     }
 
-    void OnSelect(){
-        SceneManager.LoadScene("RedeemCode");
+    void OnA(){
+        switch(selectedMenu){
+            case "Minigames" : 
+                SceneManager.LoadScene("MinigamesScreen");
+            break;
+            case "Tournament" : 
+                SceneManager.LoadScene("TournamentScreen");
+            break;
+            case "Quit" : 
+                SceneManager.LoadScene("SplashScreen");
+            break;
+        }
     }
 }
