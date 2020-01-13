d99d156cdf50736d11832e16ac96cfa792a9279e
AnthoCuttivet <anthony.cuttivet@gmail.com>
2020-01-13 14:06:49 +0100

    Alpha leaderboard

--- Assets/Scripts/VictoryScreen/MinigameVSManager.cs
+++ Assets/Scripts/VictoryScreen/MinigameVSManager.cs
@@ -10,11 +10,7 @@ public class MinigameVSManager : MonoBehaviour
 
     public GameObject lines;
     public GameObject BG;
-    public Sprite deceivedBG;
-    public Sprite DFBG;
-    public Sprite KTBBG;
-    public GameObject winnerModel;
-    public GameObject banner;
+    public Sprite[] backgrounds;
     private bool generated = false;
 
     void OnEnable(){
@@ -37,13 +33,13 @@ public class MinigameVSManager : MonoBehaviour
             //Set BG
             switch(PlayersManager.instance.currentMinigame.ToString()){
                 case "Deceived" : 
-                    BG.GetComponent<Image>().sprite = deceivedBG;
+                    BG.GetComponent<Image>().sprite = backgrounds[0];
                 break;
                 case "DF" : 
-                    BG.GetComponent<Image>().sprite = DFBG;
+                    BG.GetComponent<Image>().sprite = backgrounds[1];
                 break;
                 case "KTB" : 
-                    BG.GetComponent<Image>().sprite = KTBBG;
+                    BG.GetComponent<Image>().sprite = backgrounds[2];
                 break;
             }
             BG.GetComponent<Image>().enabled = true;
@@ -51,19 +47,26 @@ public class MinigameVSManager : MonoBehaviour
             //Set winner skin and banner
             int winnerSkinID = PlayersManager.instance.globalRanking[PlayersManager.instance.currentMinigame].First().Key.Skin;
 
-            foreach(Transform t in winnerModel.transform.GetChild(0)){
+/*             foreach(Transform t in winnerModel.transform.GetChild(0)){
                 if(t.name != "Chibi_Character"){
                     t.GetComponent<SkinnedMeshRenderer>().material = GetComponent<Magesnames>().skins[winnerSkinID];
                 }
-            }
-            winnerModel.SetActive(true);
-            banner.GetComponent<Image>().sprite = GetComponent<Magesnames>().banners[winnerSkinID];
+            } */
+            //winnerModel.SetActive(true);
+            //banner.GetComponent<Image>().sprite = GetComponent<Magesnames>().banners[winnerSkinID];
             
             //Fill scoreboard
             int lineNumber = 0;
+
+/*             foreach(KeyValuePair<PlayersManager.Minigames,Dictionary<Player,int>> gr in PlayersManager.instance.globalRanking){
+                foreach (KeyValuePair<Player,int> ranking in gr){
+                    
+                }
+            } */
+
+
             foreach(KeyValuePair<Player,int> ranking in PlayersManager.instance.globalRanking[PlayersManager.instance.currentMinigame]){
                 GameObject currentLine = lines.transform.GetChild(lineNumber).gameObject;
-                currentLine.transform.Find("Icon").GetComponent<Image>().sprite = GetComponent<DebugIcons>().icons[ranking.Key.Skin];
                 string completeName = currentLine.transform.Find("PID").GetComponent<TextMeshProUGUI>().text + (ranking.Key.Id+1);
                 currentLine.transform.Find("PID").GetComponent<TextMeshProUGUI>().text = "<color=#" + ColorUtility.ToHtmlStringRGB(GetComponent<Magesnames>().playerColors[ranking.Key.Id]) + ">" + completeName + "</color>";
                 currentLine.transform.Find("Score").GetComponent<TextMeshProUGUI>().text = ranking.Value + " pts";
